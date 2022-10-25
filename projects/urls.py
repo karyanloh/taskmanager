640544084f151fd414b7d94349fe7a7bf8ca81cb
stephanie <karyansloh@gmail.com>
2022-10-24 22:37:19 -0700

    Feature 6 Complete

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import project_list
 
 urlpatterns = [
-    path("projects/", project_list, name="list_projects"),
+    path("projects/", project_list, name="project_list"),
 ]
